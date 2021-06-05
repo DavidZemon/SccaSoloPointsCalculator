34b6620fa4cab62429283086aca24636510b3412
David Zemon <david@zemon.name>
2021-06-05 17:00:19 -0500

    Add function to calculate championship points for an individual

--- championship.py
+++ championship.py
@@ -2,6 +2,7 @@
 
 import math
 import sys
+from typing import *
 
 
 def main() -> None:
@@ -13,6 +14,17 @@ def calculate_class_points(fastest: float, actual: float) -> int:
         return 10000
     else:
         return round((fastest / actual) * 10_000)
+
+
+def calculate_championship_points(points: List[Optional[int]]) -> int:
+    event_count = len(points)
+    points = [0 if event_points is None else event_points for event_points in points]
+    if event_count < 4:
+        return sum([0 if event_points is None else event_points for event_points in points])
+    else:
+        events_to_count = round(event_count / 2) + 2
+        sorted_points = sorted(points, reverse=True)
+        return sum(sorted_points[:events_to_count])
 
 
 if __name__ == '__main__':
