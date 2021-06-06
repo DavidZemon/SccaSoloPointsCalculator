47a8571efc7ae2cf10b009f4381459a41567c9a9
David Zemon <david@zemon.name>
2021-06-06 11:21:01 -0500

    Add fresh React/TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>SCCA Solo Points Calculator</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
