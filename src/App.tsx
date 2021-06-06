0d58fcfa31dba8a26edda5e932afaca262d156de
David Zemon <david@zemon.name>
2021-06-06 16:46:49 -0500

    Can now fully parse CSV file and display results in web UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { ChangeEvent, Component, ComponentPropsWithoutRef } from 'react';
 import 'bootswatch/dist/darkly/bootstrap.css';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap';
 import { ClassResultsProcessor } from './services';
+import {
+  ClassCategoryResults,
+  ClassResults,
+  EventResults,
+  LapTime,
+} from './models';
 
 interface AppState {
   eventResultsFile?: File;
+  results?: EventResults;
 }
 
 class App extends Component<ComponentPropsWithoutRef<any>, AppState> {
   private readonly processor = new ClassResultsProcessor();
-  constructor(props: Readonly<React.ComponentPropsWithoutRef<any>>) {
+  constructor(props: Readonly<ComponentPropsWithoutRef<any>>) {
     super(props);
     this.state = {};
   }
@@ -50,17 +57,112 @@ class App extends Component<ComponentPropsWithoutRef<any>, AppState> {
               disabled={this.state.eventResultsFile === undefined}
               variant={'primary'}
               onClick={async () => {
-                await this.processor.process(
+                const results = await this.processor.process(
                   await this.state.eventResultsFile!.text(),
                 );
+                this.setState({ results });
               }}
             >
               Process
             </Button>
           </Col>
         </Row>
+
+        {this.displayResults()}
       </Container>
     );
+  }
+
+  private displayResults(): JSX.Element[] {
+    return Object.entries(this.state.results || []).map(
+      ([classCategory, categoryResults], index) => (
+        <Row key={index}>
+          <Col>
+            <h2>{classCategory}</h2>
+            {this.displayCategoryResults(categoryResults)}
+          </Col>
+        </Row>
+      ),
+    );
+  }
+
+  private displayCategoryResults(
+    categoryResults: ClassCategoryResults,
+  ): JSX.Element[] {
+    return Object.values(categoryResults)
+      .map((classResults) => ({
+        ...classResults,
+        results: classResults.results.filter(
+          (driverResults) => driverResults.times.length,
+        ),
+      }))
+      .filter((classResults) => classResults.results.length)
+      .sort((a, b) => {
+        if (a.carClass < b.carClass) return -1;
+        if (a.carClass > b.carClass) return 1;
+        else return 0;
+      })
+      .map((classResults, index) => (
+        <Table key={index} striped hover borderless>
+          <thead>
+            <tr>
+              <th colSpan={10}>
+                {classResults.carClass} (Trophies: {classResults.trophyCount})
+              </th>
+            </tr>
+            <tr>
+              {ClassResultsProcessor.HEADER.slice(0, 6).map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+              <th colSpan={7}>Lap Times</th>
+              <th>Fastest</th>
+              <th>Difference</th>
+            </tr>
+          </thead>
+          <tbody>{this.displayClassResults(classResults)}</tbody>
+        </Table>
+      ));
+  }
+
+  private displayClassResults(classResults: ClassResults): JSX.Element[] {
+    const bestLapInClass = [...classResults.results[0].times].sort(
+      LapTime.compare,
+    )[0].time;
+    return classResults.results.map((driver, index) => {
+      const driverBestLap = [...driver.times].sort(LapTime.compare)[0].time;
+      return (
+        <tr key={index}>
+          <td>{driver.trophy ? 'T' : ''}</td>
+          <td>{driver.rookie ? 'R' : ''}</td>
+          <td>{driver.position}</td>
+          <td>{driver.carNumber}</td>
+          <td>{driver.name}</td>
+          <td>{driver.carDescription}</td>
+          {driver.times.map((lapTime, index) => (
+            <td key={index}>{App.displayLapTime(lapTime)}</td>
+          ))}
+          {new Array(7 - driver.times.length).fill(null).map((_, index) => (
+            <td key={index} />
+          ))}
+          <td>{driverBestLap}</td>
+          <td>
+            {bestLapInClass === driverBestLap
+              ? ''
+              : `(${(bestLapInClass! - driverBestLap!).toFixed(3)})`}
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  private static displayLapTime(lapTime: LapTime): string {
+    if (lapTime.dnf) {
+      return 'DNF';
+    } else if (lapTime.rerun) {
+      return 'Re-run';
+    } else {
+      return `${lapTime.time}` + (lapTime.cones ? `(${lapTime.cones})` : '');
+    }
   }
 }
 
--- src/services/ClassResultsProcessor.ts
+++ src/services/ClassResultsProcessor.ts
@@ -1,8 +1,14 @@
 import parse from 'csv-parse/lib/sync';
-import { EventResults } from '../models';
+import {
+  ClassCategoryResults,
+  ClassResults,
+  EventResults,
+  IndividualResults,
+  LapTime,
+} from '../models';
 
 export class ClassResultsProcessor {
-  private static readonly HEADER = [
+  public static readonly HEADER = [
     'TR',
     'RK',
     'Pos',
@@ -16,19 +22,124 @@ export class ClassResultsProcessor {
   ];
 
   async process(fileContents: string): Promise<EventResults> {
-    const lines: string[][] = parse(fileContents, {
+    const rows: string[][] = parse(fileContents, {
       columns: false,
       ltrim: true,
       rtrim: true,
       relaxColumnCount: true,
     });
 
-    lines.forEach((line) => {
-      console.log(`Line: ${JSON.stringify(line)}`);
+    const finalLines = [];
+    const eventResults: EventResults = {};
+    let classCategoryResults: ClassCategoryResults;
+    let currentClass: ClassResults;
+    rows.forEach((row) => {
+      if (row[row.length - 1].endsWith('Category')) {
+        // Just the class category
+        const categoryName = row[row.length - 1];
+        classCategoryResults = {};
+        eventResults[categoryName] = classCategoryResults;
+        finalLines.push([categoryName]);
+      } else if (row.length === 65) {
+        // Class + table header + first row
+        const classHeader = row.slice(11, 15);
+        const className = classHeader[0];
+        currentClass = new ClassResults(className);
+        classCategoryResults[className] = currentClass;
+        finalLines.push(classHeader);
+
+        finalLines.push(ClassResultsProcessor.HEADER);
+
+        const firstHalf = row.slice(15, 27);
+        const secondHalf = row.slice(33, row.length - 4);
+        finalLines.push(
+          ClassResultsProcessor.processResultsRow(
+            firstHalf.concat(secondHalf),
+            currentClass,
+          ),
+        );
+      } else if (row[0] === 'Results') {
+        const firstHalf = row.slice(11, 23);
+        const secondHalf = row.slice(29, row.length - 4);
+        finalLines.push(
+          ClassResultsProcessor.processResultsRow(
+            firstHalf.concat(secondHalf),
+            currentClass,
+          ),
+        );
+      } else {
+        // Class + table header + first row (when missing extra header prefix)
+        const classHeader = row.slice(0, 4);
+        const classname = classHeader[0];
+        currentClass = new ClassResults(classname);
+        classCategoryResults[classname] = currentClass;
+        finalLines.push(classHeader);
+
+        finalLines.push(ClassResultsProcessor.HEADER);
+        const firstHalf = row.slice(4, 16);
+        const secondHalf = row.slice(22, row.length - 4);
+        finalLines.push(
+          ClassResultsProcessor.processResultsRow(
+            firstHalf.concat(secondHalf),
+            currentClass,
+          ),
+        );
+      }
     });
-
-    const eventResults = {};
-
     return eventResults;
+  }
+
+  static processResultsRow(
+    row: string[],
+    classResults?: ClassResults,
+  ): string[] {
+    if (!classResults)
+      throw Error(
+        `Class results object is uninitialized for row ${row.join(',')}`,
+      );
+    else {
+      const indexOfFastest = 11;
+      const indexOfDifference = 15;
+      const fastest = row[indexOfFastest];
+      const difference = row[indexOfDifference];
+
+      const fixedRow = row
+        .slice(0, indexOfFastest)
+        .concat(row.slice(indexOfFastest + 1, indexOfDifference))
+        .concat(row.slice(indexOfDifference + 1, 24));
+
+      fixedRow[20] = fastest;
+      fixedRow[21] = difference;
+
+      this.appendIndividualResults(fixedRow, classResults);
+
+      return fixedRow;
+    }
+  }
+
+  static appendIndividualResults(
+    fixedRow: string[],
+    classResults: ClassResults,
+  ): void {
+    if (fixedRow[0] === 'T') {
+      classResults.trophyCount += 1;
+    }
+    const individualResults: IndividualResults = {
+      id: fixedRow[4], // FIXME
+      carClass: classResults.carClass,
+      trophy: fixedRow[0] === 'T',
+      rookie: fixedRow[1] === 'R',
+      position: parseFloat(fixedRow[2]),
+      carNumber: parseInt(fixedRow[3]),
+      name: fixedRow[4],
+      carDescription: fixedRow[5],
+      times: fixedRow
+        .slice(8, 20)
+        .filter((lapTime) => lapTime.trim())
+        .map((lapTime) => new LapTime(lapTime)),
+    };
+    if (individualResults.times.length) {
+      classResults.results.push(individualResults);
+    }
   }
 }
