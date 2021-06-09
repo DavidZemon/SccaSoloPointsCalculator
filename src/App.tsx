a906e82244aea467a13ef698b7809b2abd194f2f
David Zemon <david@zemon.name>
2021-06-09 10:04:26 -0500

    Add a touch of input validation and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { ChangeEvent, Component, ComponentPropsWithoutRef } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootswatch/dist/darkly/bootstrap.css';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 import { ClassResultsProcessor } from './services';
 import { EventResults } from './models';
 import { EventResults as EventResultsComponent } from './components/EventResults';
 import { PaxService } from './services/PaxService';
+import { EOL } from 'os';
+import assert from 'assert';
+import { ToastContainer } from 'react-toastify';
 
 interface AppState {
   eventResultsFile?: File;
@@ -30,87 +35,116 @@ class App extends Component<ComponentPropsWithoutRef<any>, AppState> {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col>
-            <h1>SCCA Solo Points Calculator</h1>
-          </Col>
-        </Row>
+      <div>
+        <ToastContainer pauseOnHover />
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1>SCCA Solo Points Calculator</h1>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col>
-            {this.state.eventResultsFile ? (
-              <p>
-                Ready to process <code>{this.state.eventResultsFile.name}</code>
-              </p>
-            ) : (
-              <Form.File
-                label="Event Class Results"
-                custom
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  if (event.target.files && event.target.files.length) {
-                    this.setState({
-                      eventResultsFile: event.target.files[0],
-                    });
-                  }
-                }}
-              />
-            )}
-          </Col>
-
-          <Col>
-            {this.state.championshipResultsFile ? (
-              this.state.eventResultsFile ? (
+          <Row>
+            <Col>
+              {this.state.eventResultsFile ? (
                 <p>
                   Ready to process{' '}
-                  <code>{this.state.championshipResultsFile.name}</code>
+                  <code>{this.state.eventResultsFile.name}</code>
                 </p>
               ) : (
-                <p>
-                  <code>{this.state.championshipResultsFile.name}</code> set as
-                  championship standings. Please add event results to begin
-                  processing.
-                </p>
-              )
-            ) : (
-              <Form.File
-                label="Championship standings"
-                custom
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  if (event.target.files && event.target.files.length) {
-                    this.setState({
-                      championshipResultsFile: event.target.files[0],
-                    });
-                  }
+                <Form.File
+                  label="Event Class Results"
+                  custom
+                  onChange={async (event: ChangeEvent<HTMLInputElement>) => {
+                    if (event.target.files && event.target.files.length) {
+                      try {
+                        await this.validateUploadedFile(event.target.files[0]);
+                        this.setState({
+                          eventResultsFile: event.target.files[0],
+                        });
+                      } catch (e) {
+                        console.error(e.message);
+                        toast.error(
+                          'File format does not match expected. Please export event results with raw times, grouped by class.',
+                        );
+                      }
+                    }
+                  }}
+                />
+              )}
+            </Col>
+
+            <Col>
+              {this.state.championshipResultsFile ? (
+                this.state.eventResultsFile ? (
+                  <p>
+                    Ready to process{' '}
+                    <code>{this.state.championshipResultsFile.name}</code>
+                  </p>
+                ) : (
+                  <p>
+                    <code>{this.state.championshipResultsFile.name}</code> set
+                    as championship standings. Please add event results to begin
+                    processing.
+                  </p>
+                )
+              ) : (
+                <Form.File
+                  label="Championship standings"
+                  custom
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    if (event.target.files && event.target.files.length) {
+                      this.setState({
+                        championshipResultsFile: event.target.files[0],
+                      });
+                    }
+                  }}
+                />
+              )}
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button
+                disabled={this.state.eventResultsFile === undefined}
+                variant={'primary'}
+                onClick={async () => {
+                  const results = await this.processor.process(
+                    await this.state.eventResultsFile!.text(),
+                  );
+                  this.setState({ results });
                 }}
-              />
-            )}
-          </Col>
-        </Row>
+              >
+                Process
+              </Button>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col>
-            <Button
-              disabled={this.state.eventResultsFile === undefined}
-              variant={'primary'}
-              onClick={async () => {
-                const results = await this.processor.process(
-                  await this.state.eventResultsFile!.text(),
-                );
-                this.setState({ results });
-              }}
-            >
-              Process
-            </Button>
-          </Col>
-        </Row>
-
-        <EventResultsComponent
-          paxService={this.paxService}
-          results={this.state.results}
-        />
-      </Container>
+          <EventResultsComponent
+            paxService={this.paxService}
+            results={this.state.results}
+          />
+        </Container>
+      </div>
     );
+  }
+
+  async validateUploadedFile(f: File) {
+    const content = await f.text();
+    const firstLine = content.split(EOL)[0];
+    const firstLineWords = firstLine.split(',');
+
+    const errorMessage = `First line does not match expected value. Actual: \`${firstLine}\``;
+
+    assert(firstLineWords[0] === '"Results"', errorMessage);
+    assert(firstLineWords[2] === '"www.ProntoTimingSystem.com"', errorMessage);
+    assert(firstLineWords[3] === '"Pos"', errorMessage);
+    assert(firstLineWords[4] === '"Nbr"', errorMessage);
+    assert(firstLineWords[5] === '"Driver\'s name', errorMessage);
+    assert(firstLineWords[6] === ' Town"', errorMessage);
+    assert(firstLineWords[7] === '"Car', errorMessage);
+    assert(firstLineWords[8] === ' Sponsor"', errorMessage);
   }
 }
 
